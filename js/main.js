17467d9fedf8a52d4f13b813420da23f96e045b0
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-01-19 22:01:42 +0300

    add about section with video

--- js/main.js
+++ js/main.js
@@ -6,3 +6,22 @@ $(document).ready(function () {
     margin: 48,
   });
 });
+
+const videoBtn = document.querySelector("#btn-video");
+const videoPicture = document.querySelector(".video__picture");
+const videoWrapper = document.querySelector(".video");
+const video = document.querySelector("#video-file");
+
+videoWrapper.addEventListener("click", function () {
+  if (video.paused) {
+    videoPicture.classList.add("none");
+    videoWrapper.classList.remove("video-overlay");
+    videoBtn.classList.add("none");
+
+    video.play();
+  } else {
+    video.pause();
+    videoWrapper.classList.add("video-overlay");
+    videoBtn.classList.remove("none");
+  }
+});
