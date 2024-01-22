47e89404a2dad3a120eb8bd8c61f4b871378585d
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-01-22 15:59:39 +0300

    add mobile nav panel

--- js/main.js
+++ js/main.js
@@ -15,14 +15,26 @@ $(document).ready(function () {
   });
 });
 
+const closeMobileNav = document.querySelector(".mobile-nav__btn");
+const openMobileNav = document.querySelector("#openMobileNav");
+const MobileNav = document.querySelector(".mobile-nav");
+
+closeMobileNav.onclick = function () {
+  MobileNav.classList.add("none");
+  document.body.classList.toggle("no-scroll");
+};
+openMobileNav.onclick = function () {
+  MobileNav.classList.remove("none");
+  document.body.classList.toggle("no-scroll");
+};
+
 const videoBtn = document.querySelector("#btn-video");
 const videoPicture = document.querySelector(".video__picture");
 const videoWrapper = document.querySelector(".video");
 const video = document.querySelector("#video-file");
-
 videoWrapper.addEventListener("click", function () {
   if (video.paused) {
-    videoPicture.classList.add("none");
+    videoPicture.classList.add("hidden");
     videoWrapper.classList.remove("video-overlay");
     videoBtn.classList.add("none");
 
